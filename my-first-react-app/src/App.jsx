e998172d0c252e59845129100ca808e6e63cb5e9
saanvi-cloud <saanvidd2003@gmail.com>
2025-08-19 05:12:13 -0700

    final frontend for the movies website using react and vite

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,95 +1,94 @@
-import React, { useState, useEffect } from 'react'
-import Search from './components/Search'
-import Spinner from './components/Spinner';
-import MovieCard from './components/MovieCard';
-import {useDebounce} from 'react-use';
+import React, { useState, useEffect } from 'react' 
+import Search from './components/Search' 
+import Spinner from './components/Spinner'; 
+import MovieCard from './components/MovieCard'; 
+import {useDebounce} from 'react-use'; 
 
-const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_BASE_URL = 'https://api.themoviedb.org/3'; 
+
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY; // ✅ must use VITE_ prefix
 
-const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [movieList, setMovieList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+const App = () => { 
+
+  const [searchTerm, setSearchTerm] = useState(''); 
+  const [errorMessage, setErrorMessage] = useState(''); 
+  const [movieList, setMovieList] = useState([]); 
+  const [isLoading, setIsLoading] = useState(false); 
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
-  useDebounce(
-    () => {
-      setDebouncedSearchTerm(searchTerm);
-    },
-    500,
-    [searchTerm] // always a single dependency
-  );
+  useDebounce( () => { 
+    setDebouncedSearchTerm(searchTerm);
+   }, 500, [searchTerm] // always a single dependency 
+  ); 
 
-  const fetchMovies = async (query = '') => {
-    setIsLoading(true);
-    setErrorMessage('');
-    try {
-      const endpoint = query ? 
-      `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}&api_key=${API_KEY}` : 
-      `${API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
-      const response = await fetch(endpoint);
 
-      if (!response.ok) {
-        throw new Error('Failed to fetch movies');
-      }
+const fetchMovies = async (query = '') => { 
 
-      const data = await response.json();
+  setIsLoading(true); 
+  setErrorMessage(''); 
 
-      if (data.success === false) {
+  try { 
+    const endpoint = query ? 
+    `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}&api_key=${API_KEY}` : 
+    `${API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+    
+    const response = await fetch(endpoint); 
+    if (!response.ok) { 
+      throw new Error('Failed to fetch movies'); 
+    } 
+    
+    const data = await response.json(); 
+    if (data.success === false) { 
       setErrorMessage(data.status_message || 'Failed to fetch movies');
-      setMovieList([]);
-      return;
-      }
-
-      setMovieList(data.results || []);
-    } catch (error) {
-      console.error(`Error fetching movies: ${error}`);
-      setErrorMessage('Error fetching movies. Please try again later.');
-    } finally{
-      setIsLoading(false);
+      setMovieList([]); 
+      return; 
     }
-  };
+    setMovieList(data.results || []); 
+  } 
+  
+  catch (error) { 
+    console.error(`Error fetching movies: ${error}`); 
+    setErrorMessage('Error fetching movies. Please try again later.');
+  } finally{ 
+    setIsLoading(false); 
+  } 
+}; 
 
-  useEffect(() => {
-    if (debouncedSearchTerm !== '') {
-      fetchMovies(debouncedSearchTerm);
-    }
-  }, [debouncedSearchTerm]);
+useEffect(() => {
+  fetchMovies(debouncedSearchTerm);
+}, [debouncedSearchTerm]);
+ 
+  
+  return ( 
+    <main> 
+      <div className="pattern" /> 
+        <div className="wrapper"> 
+          <header> 
+            <img src="./hero.png" alt="Hero Banner" /> 
+            <h1> Find 
+              <span className="text-gradient"> Movies </span> 
+              You'll Enjoy Without the Hassle 
+            </h1> 
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} /> 
+          </header> 
+          <section className="all-movies"> 
+            <h2 className='mt-[40px]'>All Movies</h2> 
+            
+            {isLoading ? 
+            ( <Spinner/> ): 
+              errorMessage ? ( <p className='text-red-500'>{errorMessage}</p> ): 
+              ( <ul> 
+                {movieList.map((movie) => ( 
+                  <MovieCard key={movie.id} movie={movie}/> 
+                ))} 
+                </ul> 
+              )
+            } 
 
-  return (
-    <main>
-      <div className="pattern" />
-      <div className="wrapper">
-        <header>
-          <img src="./hero.png" alt="Hero Banner" />
-          <h1>
-            Find <span className="text-gradient"> Movies </span>
-            You'll Enjoy Without the Hassle
-          </h1>
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        </header>
-
-        <section className="all-movies">
-          <h2 className='mt-[40px]'>All Movies</h2>
-          {isLoading ? (
-            <Spinner/>
-          ): errorMessage ? (
-            <p className='text-red-500'>{errorMessage}</p>
-          ): (
-            <ul>
-              {movieList.map((movie) => (
-                <MovieCard key={movie.id} movie={movie}/>
-              ))}
-            </ul>
-          )}
-        </section>
-
-        <h1 className="text-white">{searchTerm}</h1>
-      </div>
-    </main>
-  );
-};
-
-export default App;
+          </section> 
+          <h1 className="text-white">{searchTerm}</h1> 
+        </div> 
+    </main> 
+    ); 
+  }; 
+  export default App;
